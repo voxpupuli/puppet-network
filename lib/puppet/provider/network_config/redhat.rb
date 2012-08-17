e82cda2591749b77414a77d5ceae21655cd0603c
Adrien Thebo <adrien@puppetlabs.com>
2012-08-16 18:15:24 -0700

    Code cleanup, add commenting

--- lib/puppet/provider/network_config/redhat.rb
+++ lib/puppet/provider/network_config/redhat.rb
@@ -26,17 +26,29 @@ Puppet::Type.type(:network_config).provide(:redhat) do
     "#{SCRIPT_DIRECTORY}/ifcfg-#{@resource.name}"
   end
 
+  # Scan all files in the networking directory for interfaces
   def self.target_files
     Dir["#{SCRIPT_DIRECTORY}/ifcfg-*"]
   end
 
+  # Convert a redhat network script into a hash
   def self.parse_file(filename, contents)
-    lines = contents.split('\n').map { |line| line.sub(/#.*$/, '') }.reject { |line| line.match(/^\s+$/) }
+    # Split up the file into lines
+    lines = contents.split('\n')
+    # Strip out all comments
+    lines.map! { |line| line.sub(/#.*$/, '') }
+    # Remove all blank lines
+    lines = lines.reject! { |line| line.match(/^\s+$/) }
 
+    # Extract all known properties
     file_properties = lines.inject({}) do |hash, line|
-      # XXX VALIDATION
-      pair = line.split('=').map(&:strip)
-      hash[pair[0]] = pair[1]
+      if (m = line.match /^(.+)=(.*)$/)
+        key = m[1].strip
+        val = m[2].strip
+        hash[key] = val
+      else
+        raise Puppet::Error, "#{filename} is malformed"
+      end
       hash
     end
 
@@ -44,8 +56,8 @@ Puppet::Type.type(:network_config).provide(:redhat) do
 
     # For each interface attribute that we recognize it, add the value to the
     # hash with our expected label
-    NAME_MAPPINGS.each_pair do |typename, redhat_name|
-      provider_properties[typename] = file_properties.delete(redhat_name)
+    NAME_MAPPINGS.each_pair do |property, redhat_name|
+      provider_properties[property] = file_properties.delete(redhat_name)
     end
 
     # For all of the remaining values, blindly toss them into the options hash.
@@ -57,23 +69,25 @@ Puppet::Type.type(:network_config).provide(:redhat) do
   end
 
   def self.format_file(filename, providers)
+    case providers.length
+    when 0
+      ''
+    when 1
+      provider = providers[0]
+      lines = []
 
-    raise Puppet::DevError, 'Unable to support multiple interfaces in a single file' if providers.length > 1
+      # :name is not an actual property, since it's the namevar. Therefore we
+      # need to handle it separately
+      lines << "DEVICE=#{@resource.name}"
+      NAME_MAPPINGS.each_pair do |typename, redhat_name|
+        lines << "#{redhat_name}=#{provider.property(typename).value}"
+      end
+      # Map any general options to key/value pairs
+      lines << options.map { |(key, val)| "#{key}=#{val}" }
 
-    provider = providers[0]
-    # XXX What to do if no providers passed?
-    lines = []
-
-    lines << "DEVICE=#{@resource.name}"
-
-    NAME_MAPPINGS.each_pair do |typename, redhat_name|
-      lines << "#{redhat_name}=#{provider.property(typename).value}"
+      lines.join('\n')
+    else
+      raise Puppet::DevError, 'Unable to support multiple interfaces in a single file' if providers.length > 1
     end
-
-    lines << options.map do |(key, val)|
-      "#{key}=#{val}"
-    end
-
-    lines.join('\n')
   end
 end
