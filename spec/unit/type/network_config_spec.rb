1c6f08b7a6dc812ad12b567d40df4cc34c79048b
Adrien Thebo <git@somethingsinistral.net>
2012-12-05 19:49:34 -0800

    Add check for hotpluggable feature, clean up tests
    `described_class` is neato.

--- spec/unit/type/network_config_spec.rb
+++ spec/unit/type/network_config_spec.rb
@@ -2,55 +2,56 @@
 
 require 'spec_helper'
 
-type_class = Puppet::Type.type(:network_config)
-
-describe type_class do
+describe Puppet::Type.type(:network_config) do
   before do
     @provider_class = stub 'provider class', :name => "fake", :suitable? => true, :supports_parameter? => true
     @provider = stub 'provider', :class => @provider_class
     @provider_class.stubs(:new).returns @provider
 
-    type_class.stubs(:defaultprovider).returns @provider_class
-    type_class.stubs(:provider).returns @provider_class
+    described_class.stubs(:defaultprovider).returns @provider_class
+    described_class.stubs(:provider).returns @provider_class
+  end
+
+  describe "feature" do
+
+    describe "hotpluggable" do
+      it { described_class.provider_feature(:hotpluggable).should_not be_nil }
+    end
   end
 
   describe "when validating the attribute" do
 
-    subject { type_class }
-
     [:name, :reconfigure].each do |param|
       it "should have the '#{param}' param" do
-        subject.attrtype(param).should == :param
+        described_class.attrtype(param).should == :param
       end
     end
 
     [:ensure, :ipaddress, :netmask, :method, :family, :onboot, :options].each do |property|
       it "should have the '#{property}' property" do
-        subject.attrtype(property).should == :property
+        described_class.attrtype(property).should == :property
       end
     end
 
     it "use the name parameter as the namevar" do
-      subject.key_attributes.should == [:name]
+      described_class.key_attributes.should == [:name]
     end
 
     describe "ensure" do
       it "should be an ensurable value" do
-        subject.propertybyname(:ensure).ancestors.should be_include(Puppet::Property::Ensure)
+        described_class.propertybyname(:ensure).ancestors.should be_include(Puppet::Property::Ensure)
       end
     end
 
     describe "options" do
       it "should be a descendant of the KeyValue property" do
         pending "on conversion to specific type"
-        subject.propertybyname(:options).ancestors.should be_include(Puppet::Property::Ensure)
+        described_class.propertybyname(:options).ancestors.should be_include(Puppet::Property::Ensure)
       end
     end
   end
 
   describe "when validating the attribute value" do
-
-    subject { type_class }
 
     describe "ipaddress" do
 
@@ -60,27 +61,27 @@ describe type_class do
       describe "using the inet family" do
 
         it "should require that a passed address is a valid IPv4 address" do
-          expect { subject.new(:name => 'yay', :family => :inet, :ipaddress => address4) }.to_not raise_error
+          expect { described_class.new(:name => 'yay', :family => :inet, :ipaddress => address4) }.to_not raise_error
         end
         it "should fail when passed an IPv6 address" do
           pending "implementation of IP address validation"
-          expect { subject.new(:name => 'yay', :family => :inet, :ipaddress => address6) }.to raise_error
+          expect { described_class.new(:name => 'yay', :family => :inet, :ipaddress => address6) }.to raise_error
         end
       end
 
       describe "using the inet6 family" do
         it "should require that a passed address is a valid IPv6 address" do
-          expect { subject.new(:name => 'yay', :family => :inet6, :ipaddress => address6) }.to_not raise_error
+          expect { described_class.new(:name => 'yay', :family => :inet6, :ipaddress => address6) }.to_not raise_error
         end
         it "should fail when passed an IPv4 address" do
           pending "implementation of IP address validation"
-          expect { subject.new(:name => 'yay', :family => :inet6, :ipaddress => address4) }.to raise_error
+          expect { described_class.new(:name => 'yay', :family => :inet6, :ipaddress => address4) }.to raise_error
         end
       end
 
       it "should fail if a malformed address is used" do
         pending "implementation of IP address validation"
-        expect { subject.new(:name => 'yay', :ipaddress => 'This is clearly not an IP address') }.to raise_error
+        expect { described_class.new(:name => 'yay', :ipaddress => 'This is clearly not an IP address') }.to raise_error
       end
     end
 
@@ -89,7 +90,7 @@ describe type_class do
       it "should fail if an invalid CIDR netmask is used" do
         pending "implementation of IP address validation"
         expect do
-          subject.new(:name => 'yay', :netmask => 'This is clearly not a netmask')
+          described_class.new(:name => 'yay', :netmask => 'This is clearly not a netmask')
         end.to raise_error
       end
     end
@@ -97,7 +98,7 @@ describe type_class do
     describe "method" do
       [:static, :manual, :dhcp].each do |mth|
         it "should consider '#{mth}' a valid configuration method" do
-          subject.new(:name => 'yay', :method => mth)
+          described_class.new(:name => 'yay', :method => mth)
         end
       end
     end
@@ -105,7 +106,7 @@ describe type_class do
     describe "family" do
       [:inet, :inet6].each do |family|
         it "should consider '#{family}' a valid address family" do
-          subject.new(:name => 'yay', :family => family)
+          described_class.new(:name => 'yay', :family => family)
         end
       end
     end
@@ -113,7 +114,7 @@ describe type_class do
     describe 'onboot' do
       [true, false].each do |bool|
         it "should accept '#{bool}' for onboot" do
-          subject.new(:name => 'yay', :onboot => bool)
+          described_class.new(:name => 'yay', :onboot => bool)
         end
       end
     end
@@ -121,7 +122,7 @@ describe type_class do
     describe 'reconfigure' do
       [true, false].each do |bool|
         it "should accept '#{bool}' for reconfigure" do
-          subject.new(:name => 'yay', :reconfigure => bool)
+          described_class.new(:name => 'yay', :reconfigure => bool)
         end
       end
     end
@@ -129,18 +130,18 @@ describe type_class do
     describe "options" do
       it "should accept an empty hash" do
         expect do
-          subject.new(:name => "valid", :options => {})
+          described_class.new(:name => "valid", :options => {})
         end.to_not raise_error
       end
 
       it "should use an empty hash as the default" do
         expect do
-          subject.new(:name => "valid")
+          described_class.new(:name => "valid")
         end.to_not raise_error
       end
       it "should fail if a non-hash is passed" do
         expect do
-          subject.new(:name => "valid", :options => "geese" )
+          described_class.new(:name => "valid", :options => "geese" )
         end.to raise_error
       end
     end
