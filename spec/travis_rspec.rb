bef0fec905232f26fc396f20a1a85600dd04fe46
Adrien Thebo <git@somethingsinistral.net>
2013-01-20 20:30:19 -0800

    Add travis_rspec.rb for puppet module deps
    Travis doesn't seem to have a good way to install Puppet module
    dependencies; this adds in a helper to do that.

---
+++ spec/travis_rspec.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+rubylib = []
+
+modulepath = %x{bundle exec puppet config print modulepath}
+
+modulepath.split(':').each { |path| rubylib += Dir.glob("#{path}/*/lib") }
+
+ENV['RUBYLIB'] = rubylib.join(':')
+
+Kernel.exec "bundle exec rspec #{ARGV.join(' ')}"
