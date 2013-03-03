cd53545cc7ce4f04922b823648d631f1dfdd8e94
Adrien Thebo <git@somethingsinistral.net>
2013-03-03 15:17:23 -0800

    Permit undef options in debian bonding options

--- manifests/bond/debian.pp
+++ manifests/bond/debian.pp
@@ -24,23 +24,27 @@ define network::bond::debian(
   $xmit_hash_policy,
 ) {
 
+  $raw = {
+    'bond-slaves'    => join($slaves, ' '),
+    'bond-mode'      => $mode,
+    'bond-miimon'    => $miimon,
+    'bond-downdelay' => $downdelay,
+    'bond-updelay'   => $updelay,
+    'bond-lacp-rate' => $lacp_rate,
+    'bond-primary'   => $primary,
+    'bond-primary-reselect' => $primary_reselect,
+    'bond-xmit-hash-policy' => $xmit_hash_policy,
+  }
+
+  $opts = compact_hash($raw)
+
   network_config { $name:
-    ensure             => $ensure,
-    ipaddress          => $ipaddress,
-    netmask            => $netmask,
-    family             => $family,
-    onboot             => $onboot,
-    options            => {
-      'bond-mode'      => $mode,
-      'bond-miimon'    => $miimon,
-      'bond-downdelay' => $downdelay,
-      'bond-updelay'   => $updelay,
-      'bond-lacp-rate' => $lacp_rate,
-      # These can't be supported yet since there isn't clean handling of `undef`
-      #'bond-primary'          => $primary,
-      #'bond-primary-reselect' => $primary_reselect,
-      'bond-xmit-hash-policy'  => $xmit_hash_policy,
-    },
+    ensure    => $ensure,
+    ipaddress => $ipaddress,
+    netmask   => $netmask,
+    family    => $family,
+    onboot    => $onboot,
+    options   => $opts,
   }
 
   network_config { $slaves:
