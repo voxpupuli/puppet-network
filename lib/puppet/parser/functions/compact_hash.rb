4caaa31264182ae2554035d5382cfda9c649019a
Adrien Thebo <git@somethingsinistral.net>
2013-03-03 15:17:09 -0800

    Add rudimentary compact_hash function

---
+++ lib/puppet/parser/functions/compact_hash.rb
@@ -0,0 +1,27 @@
+Puppet::Parser::Functions.newfunction(:compact_hash,
+                                      :type  => :rvalue,
+                                      :arity => 1,
+                                      :doc   => <<-EOD) do |args|
+  compact_hash
+  ============
+
+  Compresses a hash to remove all elements whose values are nil or undef.
+
+  Examples
+  --------
+
+  $example = {
+    'one'  => 'two',
+    'red'  => undef,
+    'blue' => nil,
+  }
+
+  compact_hash($example)
+  # => { 'one => 'two' }
+
+  EOD
+
+  hash = args[0]
+
+  hash.reject { |_, val| val.nil? or val == :undef }
+end
