47bff902f16259f65324e88e018a3f2ced0538d6
Adrien Thebo <git@somethingsinistral.net>
2013-03-03 14:55:49 -0800

    Add support for debian bonding options

--- manifests/bond/debian.pp
+++ manifests/bond/debian.pp
@@ -2,7 +2,9 @@
 #
 # Instantiate bonded interfaces on Debian based systems.
 #
-# ==
+# == See also
+#
+# * Debian Network Bonding http://wiki.debian.org/Bonding
 define network::bond::debian(
   $slaves,
   $ensure    = present,
@@ -23,11 +25,22 @@ define network::bond::debian(
 ) {
 
   network_config { $name:
-    ensure    => $ensure,
-    ipaddress => $ipaddress,
-    netmask   => $netmask,
-    family    => $family,
-    onboot    => $onboot,
+    ensure             => $ensure,
+    ipaddress          => $ipaddress,
+    netmask            => $netmask,
+    family             => $family,
+    onboot             => $onboot,
+    options            => {
+      'bond-mode'      => $mode,
+      'bond-miimon'    => $miimon,
+      'bond-downdelay' => $downdelay,
+      'bond-updelay'   => $updelay,
+      'bond-lacp-rate' => $lacp_rate,
+      # These can't be supported yet since there isn't clean handling of `undef`
+      #'bond-primary'          => $primary,
+      #'bond-primary-reselect' => $primary_reselect,
+      'bond-xmit-hash-policy'  => $xmit_hash_policy,
+    },
   }
 
   network_config { $slaves:
