b168c80b241f9824dbb3ae9f2e28369006e728f7
Adrien Thebo <git@somethingsinistral.net>
2013-03-03 18:37:22 -0800

    Add test coverage for debian, more for redhat

--- spec/defines/bond/debian_spec.rb
+++ spec/defines/bond/debian_spec.rb
@@ -49,4 +49,46 @@ describe 'network::bond::debian', :type => :define do
       })
     end
   end
+
+  describe "with non-default bonding params" do
+    let(:params) do
+      {
+        'ensure'    => 'present',
+        'method'    => 'static',
+        'ipaddress' => '10.20.2.1',
+        'netmask'   => '255.255.255.192',
+        'slaves'    => ['eth0', 'eth1', 'eth2'],
+
+        'mode'             => 'balance-rr',
+        'miimon'           => '50',
+        'downdelay'        => '100',
+        'updelay'          => '100',
+        'lacp_rate'        => 'fast',
+        'xmit_hash_policy' => 'layer3+4',
+      }
+    end
+    ['eth0', 'eth1', 'eth2'].each do |slave|
+      it "should add a network_config resource for #{slave}" do
+        should contain_network_config(slave).with_ensure('absent')
+      end
+    end
+
+    it "should add a network_config resource for bond0" do
+      should contain_network_config('bond0').with({
+        'ensure'         => 'present',
+        'method'         => 'static',
+        'ipaddress'      => '10.20.2.1',
+        'netmask'        => '255.255.255.192',
+        'options'        => {
+          'bond-slaves'           => 'eth0 eth1 eth2',
+          'bond-mode'             => 'balance-rr',
+          'bond-miimon'           => '50',
+          'bond-downdelay'        => '100',
+          'bond-updelay'          => '100',
+          'bond-lacp-rate'        => 'fast',
+          'bond-xmit-hash-policy' => 'layer3+4',
+        },
+      })
+    end
+  end
 end
--- spec/defines/bond/redhat_spec.rb
+++ spec/defines/bond/redhat_spec.rb
@@ -49,4 +49,48 @@ describe 'network::bond::redhat', :type => :define do
       })
     end
   end
+
+  describe "with non-default bonding params" do
+    let(:params) do
+      {
+        'ensure'    => 'present',
+        'method'    => 'static',
+        'ipaddress' => '10.20.2.1',
+        'netmask'   => '255.255.255.192',
+        'slaves'    => ['eth0', 'eth1', 'eth2'],
+
+        'mode'             => 'balance-rr',
+        'miimon'           => '50',
+        'downdelay'        => '100',
+        'updelay'          => '100',
+        'lacp_rate'        => 'fast',
+        'xmit_hash_policy' => 'layer3+4',
+      }
+    end
+    ['eth0', 'eth1', 'eth2'].each do |slave|
+      it "should add a network_config resource for #{slave}" do
+        should contain_network_config(slave).with({
+          'ensure' => 'present',
+          'method' => 'static',
+          'onboot' => true,
+          'options'  => {
+            'MASTER' => 'bond0',
+            'SLAVE'  => 'yes',
+          },
+        })
+      end
+    end
+
+    it "should add a network_config resource for bond0" do
+      should contain_network_config('bond0').with({
+        'ensure'         => 'present',
+        'method'         => 'static',
+        'ipaddress'      => '10.20.2.1',
+        'netmask'        => '255.255.255.192',
+        'options'        => {
+          'BONDING_OPTS' => 'mode=balance-rr miimon=50 downdelay=100 updelay=100 lacp_rate=fast xmit_hash_policy=layer3+4',
+        },
+      })
+    end
+  end
 end
