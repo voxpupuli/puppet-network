4b77373c12025a796a1548ec123c0ba6248266d6
Adrien Thebo <git@somethingsinistral.net>
2013-03-03 16:49:14 -0800

    Add rspec-puppet coverage for network::bond::redhat

---
+++ spec/defines/bond/redhat_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe 'network::bond::redhat', :type => :define do
+  let(:title) { 'bond0' }
+
+  describe "with default bonding params" do
+    let(:params) do
+      {
+        'ensure'    => 'present',
+        'method'    => 'static',
+        'ipaddress' => '172.18.1.2',
+        'netmask'   => '255.255.128.0',
+        'slaves'    => ['eth0', 'eth1'],
+
+        'mode'             => 'active-backup',
+        'miimon'           => '100',
+        'downdelay'        => '200',
+        'updelay'          => '200',
+        'lacp_rate'        => 'slow',
+        'primary'          => 'eth0',
+        'primary_reselect' => 'always',
+        'xmit_hash_policy' => 'layer2',
+      }
+    end
+
+    ['eth0', 'eth1'].each do |slave|
+      it do
+        should contain_network_config(slave).with({
+          'ensure' => 'present',
+          'method' => 'static',
+          'onboot' => true,
+          'options'  => {
+            'MASTER' => 'bond0',
+            'SLAVE'  => 'yes',
+          },
+        })
+      end
+    end
+
+    it do
+      should contain_network_config('bond0').with({
+        'ensure'         => 'present',
+        'method'         => 'static',
+        'ipaddress'      => '172.18.1.2',
+        'netmask'        => '255.255.128.0',
+        'options'        => {
+          'BONDING_OPTS' => 'mode=active-backup miimon=100 downdelay=200 updelay=200 lacp_rate=slow primary=eth0 primary_reselect=always xmit_hash_policy=layer2',
+        },
+      })
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'puppet'
+require 'rspec-puppet'
 require 'mocha'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
@@ -6,4 +7,14 @@ $LOAD_PATH.unshift(File.join(PROJECT_ROOT, "lib"))
 
 RSpec.configure do |config|
   config.mock_with :mocha
+end
+
+# ---
+# Configuration for puppet-rspec
+
+fixture_path = File.expand_path('fixtures', File.dirname(__FILE__))
+
+RSpec.configure do |c|
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
