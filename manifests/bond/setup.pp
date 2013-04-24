66443f1d68cf2e2c12c33fb4e5e00530952175c2
Vlastimil Holer <holer@ics.muni.cz>
2013-04-24 13:45:15 +0200

    Explicitly set top-scope for facts

--- manifests/bond/setup.pp
+++ manifests/bond/setup.pp
@@ -1,6 +1,6 @@
 class network::bond::setup {
 
-  case $osfamily {
+  case $::osfamily {
     RedHat: {
       # Redhat installs the ifenslave command with the iputils package which
       # is available by default
