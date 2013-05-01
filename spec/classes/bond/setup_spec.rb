add662bdb298c24533464a05f0621eec99db3ac7
Adrien Thebo <git@somethingsinistral.net>
2013-05-01 16:42:23 -0700

    (network-#34) Update ifenslave package in specs

--- spec/classes/bond/setup_spec.rb
+++ spec/classes/bond/setup_spec.rb
@@ -4,6 +4,6 @@ describe 'network::bond::setup', :type => :class do
   describe 'on Debian' do
     let(:facts) {{:osfamily => 'Debian'}}
 
-    it { should contain_package('ifenslave') }
+    it { should contain_package('ifenslave-2.6') }
   end
 end
