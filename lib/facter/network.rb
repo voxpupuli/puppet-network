34259686330aab96720beb80712ee918d4ffabe3
Jordi Clariana <jclariana@trovit.com>
2015-03-18 17:51:27 +0100

    Delete not necessary facter

--- lib/facter/network.rb
+++ lib/facter/network.rb
@@ -2,16 +2,6 @@ require 'facter'
 require 'open-uri'
 require 'timeout'
 
-#Public IP
-# Expected output: The public ipaddress of this node.
-Facter.add("network_public_ip") do
-  setcode do
-    Timeout::timeout(2) do
-      open('http://ip-echo.appspot.com', 'User-Agent' => 'Ruby/Facter').read.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
-    end
-  end
-end
-
 #Gateway
 # Expected output: The ip address of the nexthop/default router
 Facter.add("network_nexthop_ip") do
