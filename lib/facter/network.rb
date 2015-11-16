4aca8d1e291cff2c3cb94d3d7c0950d67f373a0e
Saz <saz@serenity.local>
2015-11-16 12:38:41 +0100

    add Gentoo support, fix ip output parsing

--- lib/facter/network.rb
+++ lib/facter/network.rb
@@ -8,11 +8,13 @@ Facter.add('network_nexthop_ip') do
   my_gw = nil
   confine :kernel => :linux
   setcode do
-    gw_address = Facter::Util::Resolution.exec('/sbin/ip route show 0/0')
-    # not all network configurations will have a nexthop.
-    # the ip tool expresses the presence of a nexthop with the word 'via'
-    if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
+    Facter::Util::Resolution.with_env('PATH' => '/bin:/sbin:/usr/bin:/usr/sbin') do
+      gw_address = Facter::Util::Resolution.exec('ip route show 0/0')
+      #not all network configurations will have a nexthop. 
+      #the ip tool expresses the presence of a nexthop with the word 'via'
+      if gw_address.include? ' via '
+        my_gw = gw_address.split(/\s+/)[2].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
+      end
     end
     my_gw
   end
@@ -23,19 +25,20 @@ end
 Facter.add('network_primary_interface') do
   confine :kernel => :linux
   setcode do
-    gw_address = Facter::Util::Resolution.exec('/sbin/ip route show 0/0')
-    # not all network configurations will have a nexthop.
-    # the ip tool expresses the presence of a nexthop with the word 'via'
-    if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
-      fun = Facter::Util::Resolution.exec("/sbin/ip route get #{my_gw}").split("\n")[0]
-      fun.split(/\s+/)[2].to_s
-    # some network configurations simply have a link that all interactions are abstracted through
-    elsif gw_address.include? 'scope link'
-      # since we have no default route ip to determine where to send 'traffic not otherwise explicitly routed'
-      # lets just use 8.8.8.8 as far as a route goes.
-      fun = Facter::Util::Resolution.exec('/sbin/ip route get 8.8.8.8').split("\n")[0]
-      fun.split(/\s+/)[2].to_s
+    Facter::Util::Resolution.with_env('PATH' => '/bin:/sbin:/usr/bin:/usr/sbin') do
+      gw_address = Facter::Util::Resolution.exec('ip route show 0/0')
+      #not all network configurations will have a nexthop. 
+      #the ip tool expresses the presence of a nexthop with the word 'via'
+      if gw_address.include? ' via '
+        my_gw = gw_address.split(/\s+/)[2].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
+        fun = Facter::Util::Resolution.exec("ip route get #{my_gw}").split("\n")[0]
+      #some network configurations simply have a link that all interactions are abstracted through  
+      elsif gw_address.include? 'scope link'
+        #since we have no default route ip to determine where to send 'traffic not otherwise explicitly routed'
+        #lets just use 8.8.8.8 as far as a route goes.
+        fun = Facter::Util::Resolution.exec("ip route get 8.8.8.8").split("\n")[0]
+      end
+      fun.split(/dev\s+([^\s]*)\s+src\s+([^\s]*)/)[1].to_s
     end
   end
 end
@@ -45,16 +48,17 @@ end
 Facter.add('network_primary_ip') do
   confine :kernel => :linux
   setcode do
-    gw_address = Facter::Util::Resolution.exec('/sbin/ip route show 0/0')
-    if gw_address.include? ' via '
-      my_gw = gw_address.split(/\s+/)[2].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
-      fun = Facter::Util::Resolution.exec("/sbin/ip route get #{my_gw}").split("\n")[0]
-      fun.split(/\s+/)[4].to_s
-    elsif gw_address.include? 'scope link'
-      # since we have no default route ip to determine where to send 'traffic not otherwise explicitly routed'
-      # lets just use 8.8.8.8 as far as a route goes and grab our IP from there.
-      fun = Facter::Util::Resolution.exec('/sbin/ip route get 8.8.8.8').split("\n")[0]
-      fun.split(/\s+/)[4].to_s
+    Facter::Util::Resolution.with_env('PATH' => '/bin:/sbin:/usr/bin:/usr/sbin') do
+      gw_address = Facter::Util::Resolution.exec('ip route show 0/0')
+      if gw_address.include? ' via '
+        my_gw = gw_address.split(/\s+/)[2].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/).to_s
+        fun = Facter::Util::Resolution.exec("ip route get #{my_gw}").split("\n")[0]
+      elsif gw_address.include? 'scope link'
+        #since we have no default route ip to determine where to send 'traffic not otherwise explicitly routed'
+        #lets just use 8.8.8.8 as far as a route goes and grab our IP from there.
+        fun = Facter::Util::Resolution.exec("ip route get 8.8.8.8").split("\n")[0]
+      end
+      fun.split(/dev\s+([^\s]*)\s+src\s+([^\s]*)/)[2].to_s
     end
   end
 end
