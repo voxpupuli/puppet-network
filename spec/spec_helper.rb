de4ccb5b8258438ab7f607186c3d1d65f8b83a6b
Tim Meusel <tim@bastelfreak.de>
2016-12-02 09:21:24 +0100

    modulesync 0.16.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
-unless RUBY_VERSION =~ %r{^1.9}
+if Dir.exist?(File.expand_path('../../lib', __FILE__)) && RUBY_VERSION !~ %r{^1.9}
   require 'coveralls'
   require 'simplecov'
   require 'simplecov-console'
@@ -12,7 +12,10 @@ unless RUBY_VERSION =~ %r{^1.9}
     Coveralls::SimpleCov::Formatter
   ]
   SimpleCov.start do
+    track_files 'lib/**/*.rb'
     add_filter '/spec'
+    add_filter '/vendor'
+    add_filter '/.vendor'
   end
 end
 
@@ -24,7 +27,7 @@ RSpec.configure do |c|
   default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
   default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
   c.default_facts = default_facts
-  c.mock_with :mocha
+  c.mock_with :rspec
 end
 
 require 'spec_helper_methods'
