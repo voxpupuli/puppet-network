9241848ae6be9ccb9241fb8e75f7a0494a72d1a0
David Hollinger <david.hollinger@moduletux.com>
2018-08-15 17:06:57 -0500

    WIP - add create tests

--- spec/unit/puppet/provider/network_route/network_route_spec.rb
+++ spec/unit/puppet/provider/network_route/network_route_spec.rb
@@ -67,13 +67,24 @@ RSpec.describe Puppet::Provider::NetworkRoute::NetworkRoute do
     end
   end
 
-  # describe 'create(context, name, should)' do
-  #   it 'creates the resource' do
-  #     expect(context).to receive(:notice).with(%r{\ACreating 'a'})
+  describe 'create(context, name, should)' do
+    let(:should) { route[0] }
 
-  #     provider.create(context, 'a', name: 'a', ensure: 'present')
-  #   end
-  # end
+    before(:each) do
+      #allow(Net::IP::Route).to receive(:new).with(should).and_return(:new_route)
+      #allow(Net::IP).to receive_message_chain("routes.new").with(new_route).and_return(nil)
+      allow(provider).to receive(:puppet_munge).with(network_route[0]).and_return(should)
+    end
+    
+    it 'creates the resource' do
+      # expect(context).to receive(:notice).with(%r{\ACreating 'a'})
+
+      # provider.create(context, 'a', name: 'a', ensure: 'present')
+      expect(Net::IP::Route).to receive(:new).with(should).and_return('')
+
+      provider.create(context, 'default', network_route[0])
+    end
+  end
 
   # describe 'update(context, name, should)' do
   #   it 'updates the resource' do
