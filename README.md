155d0027a5a8c7ff85f8097be2eafe9ae2aa265c
Jan Vanmullem <jan.vanmullem@infrabel.be>
2022-11-23 16:51:30 +0100

    replace the ipaddress gem with the ruby built-in ipaddr class

--- README.md
+++ README.md
@@ -111,10 +111,8 @@ This module requires the FileMapper mixin, available at <https://github.com/voxp
 The network_config type requires the Boolean mixin, available at <https://github.com/adrienthebo/puppet-boolean>.
 
 The debian routes provider requires the package [ifupdown-extra](http://packages.debian.org/search?suite=all&section=all&arch=any&searchon=names&keywords=ifupdown-extra).
-The `network_config` class requires the `ipaddress` gem, which needs to be
-installed on both the puppet master and the nodes. `ifupdown-extra` and
-`ipaddress` can be installed automatically using the `network` class. To use it,
-include it like so in your manifests:
+`ifupdown-extra` and `ipaddress` can be installed automatically using the `network` class.
+To use it, include it like so in your manifests:
 
 ```puppet
 include '::network'
--- manifests/init.pp
+++ manifests/init.pp
@@ -28,40 +28,12 @@
 #
 # Default: present
 #
-# [*ipaddress*]
-#
-# The name of the ipaddress gems
-#
-# Default: ipaddress
-#
-# [*ipaddress_provider*]
-#
-# The provider of the ipaddress gem
-#
-# Default: gem
-#
-# [*manage_ipaddress*]
-#
-# Whether this class should manage the ipaddress gem
-#
-# Default: true
-#
-# [*ensure_ipaddress*]
-#
-# What state the ifupdown-extra package should be in
-#
-# Default: present
-#
 
 class network (
   $ifupdown_extra          = 'ifupdown-extra',
   $ifupdown_extra_provider = undef,
   $manage_ifupdown_extra   = true,
   $ensure_ifupdown_extra   = present,
-  $ipaddress               = 'ipaddress',
-  $ipaddress_provider      = 'puppet_gem',
-  $manage_ipaddress        = true,
-  $ensure_ipaddress        = present,
 ) {
   if $facts['os']['family'] == 'Debian' and $manage_ifupdown_extra {
     package { $ifupdown_extra:
@@ -69,13 +41,5 @@ class network (
       provider => $ifupdown_extra_provider,
     }
     Package[$ifupdown_extra] -> Network_route <| |>
-  }
-
-  if $manage_ipaddress {
-    package { $ipaddress:
-      ensure   => $ensure_ipaddress,
-      provider => $ipaddress_provider,
-    }
-    Package[$ipaddress] -> Network_config <| |>
   }
 }
