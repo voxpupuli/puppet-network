6f98ee81b8fb53132bb99b7ac9e6c507cab13dd1
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-13 18:58:27 -0400

    replace deprecated calls with Facter::Core::Execution
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/network.rb
+++ lib/facter/network.rb
@@ -2,10 +2,10 @@ require 'facter'
 
 Facter.add(:network_nexthop_ip) do
   confine kernel: 'Linux'
-  confine { Facter::Util::Resolution.which('ip') }
+  confine { Facter::Core::Execution.which('ip') }
   my_gw = nil
   setcode do
-    gw_address = Facter::Util::Resolution.exec('ip route show 0/0')
+    gw_address = Facter::Core::Execution.execute('ip route show 0/0')
     my_gw = gw_address.split(%r{\s+})[2].match(%r{^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$}).to_s if gw_address.include? ' via '
     my_gw
   end
